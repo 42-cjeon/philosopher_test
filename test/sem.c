0526a0d31a1e9d7e367d3fb58acf8447672b79d6
Changmin Jeon <cjeon@c10r8s3.42seoul.kr>
2021-12-16 18:21:06 +0900

    test code

--- test/sem.c
+++ test/sem.c
@@ -8,10 +8,15 @@
 int main(void)
 {
 	sem_t *sem;
-	
-	sem = sem_open("forks", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 10);
+
+	sem_unlink("/ft_forks");
+	sem = sem_open("/ft_forks", O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 10);
+	for (int i = 0; i < 90; i++)
+		sem_post(sem);
+	for (int i = 0; i < 101; i++)
+		sem_wait(sem);
 
 	sem_close(sem);
-	sem_unlink("forks");
+	sem_unlink("/ft_forks");
 	return 0;
 }
